6928a02c1932b1c714b5da3f5856477be8e72e60
Elchin901 <73276152+Elchin901@users.noreply.github.com>
2023-04-06 13:44:28 +0200

    ok

--- project3/src/App.js
+++ project3/src/App.js
@@ -14,7 +14,6 @@ function App() {
             Hello world!
         </h1>,
         <div>
-            salam
             <Header />
             <HeroSection />
             <MobileApp />
--- project3/src/components/Header.js
+++ project3/src/components/Header.js
@@ -1,6 +1,16 @@
 
 export default function Header() {
   return (
-    <div className="bg-brand-color">Header</div>
+    <div className="bg-brand-color">
+      <div className="container mx-auto h-11 flex items-center">
+        <a href="#">
+          Header
+        </a>
+
+        <nav className="flex gap-x-8 text-sm font-semibold">
+        </nav>
+
+      </div>
+    </div>
   )
 }
