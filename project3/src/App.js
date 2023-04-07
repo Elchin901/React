09de1cf698591027a51989224351679c096eeed3
Elchin901 <73276152+Elchin901@users.noreply.github.com>
2023-04-07 07:14:46 +0200

    ok

--- project3/src/App.js
+++ project3/src/App.js
@@ -10,9 +10,6 @@ import Footer from 'components/Footer'
 
 function App() {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>,
         <div>
             <Header />
             <HeroSection />
