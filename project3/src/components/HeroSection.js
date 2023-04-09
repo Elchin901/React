3d99df16fd38abdef0beec75fe6d8206cf1ce47c
Elchin901 <73276152+Elchin901@users.noreply.github.com>
2023-04-09 07:45:00 +0200

    ok

--- project3/src/components/HeroSection.js
+++ project3/src/components/HeroSection.js
@@ -15,7 +15,7 @@ export default function HeroSection() {
 
 
   return (
-    <div className="">
+    <div className="relative before:bg-gradient-to-r before:from-primary-brand-color before:to-transparent before:absolute before:inset-0 before::w-full before:h-full before:z-10">
       <Slider {...settings}>
         <div>
           <img className="w-full h-[500px] object-cover" src="https://i0.wp.com/www.middleeastmonitor.com/wp-content/uploads/2021/08/20210814_2_49606971_67906843.jpg?quality=85&strip=all&zoom=1&ssl=1" />
@@ -24,7 +24,7 @@ export default function HeroSection() {
           <img className="w-full h-[500px] object-cover" src="https://www.igpdecaux.it/sites/default/files/styles/gallery_formatter_slide_2/public/case_history/getir_17-01-2022_full-wrap_darsena_milano_q2_08.jpg?itok=oyawb44j" />
         </div>
         <div>
-          <h3>3</h3>
+          <img className="w-full h-[500px] object-cover" src="https://www.tomnorwell.com/wp-content/uploads/2022/01/Getir-header-fallback.jpg"/>
         </div>
       </Slider>
     </div>
