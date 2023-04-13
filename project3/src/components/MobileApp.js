b361376467f12bcd927d42215cdfcd56e0b9d965
Elchin901 <73276152+Elchin901@users.noreply.github.com>
2023-04-13 09:30:40 +0200

    ok

--- project3/src/components/MobileApp.js
+++ project3/src/components/MobileApp.js
@@ -1,25 +1,24 @@
 
 export default function MobileApp() {
   return (
-    <div className="bg-mobile-app p-10">
-      <div>
-        <h3>Getir'i indirin</h3>
-        <p>isediginiz urunleri dakikalar icinde kapiniza getirelim.</p>
-        <nav>
-          <a href="#">
+    <div className="bg-primary-brand-color bg-mobile-app flex justify-between items-center mt-7 rounded-lg text-white">
+      <div className="flex flex-col gap-y-3 p-10">
+        <h3 className="text-2xl font-bold tracking-tight">Getir'i indirin</h3>
+        <p className="font-semibold ">isediginiz urunleri dakikalar icinde kapiniza<br/> getirelim.</p>
+        <nav className="mt-5 flex  gap-x-2">
+          <a href="#" className="transition-all transform  hover:scale-105">
             <img
               src="https://getir.com/_next/image?url=https%3A%2F%2Flanding-strapi-images-development.s3.eu-west-1.amazonaws.com%2Fappstorees_3e5b4c3264.svg&w=256&q=75" />
           </a>
-          <a href="#">
-            <img
-              src="https://getir.com/_next/image?url=https%3A%2F%2Flanding-strapi-images-development.s3.eu-west-1.amazonaws.com%2Fplaystorees_82f027bb11.svg&w=256&q=75" />
-          </a>
-          <a href="#">
+          <a href="#" className="transition-all transform  hover:scale-105">
             <img
               src="https://getir.com/_next/image?url=https%3A%2F%2Flanding-strapi-images-development.s3.eu-west-1.amazonaws.com%2Fplaystorees_82f027bb11.svg&w=256&q=75" />
           </a>
         </nav>
       </div>
+      <picture className="pt-8 px-2">
+					<img src="https://getir.uk/_next/image/?url=https%3A%2F%2Flanding-strapi-images-development.s3.eu-west-1.amazonaws.com%2Fcta_phone_def0b92214.png&w=384&q=75"/>
+      </picture>
     </div>
   )
 }
--- project3/tailwind.config.js
+++ project3/tailwind.config.js
@@ -8,12 +8,10 @@ module.exports = {
         '0.1': '0.063rem'
       },
 
-      // backgroundImage: {
-      //   'mobile-app': "url('https://tailwindcss.com/_next/static/media/tailwindui-small@75.8bb955b2.jpg')",
-      // },
-      backgroundImage: theme => ({
-        'mobile-app': 'url(https://landing-strapi-images-development.s3.eu-west-1.amazonaws.com/0.1.0/_next/static/media/header-background.9ad4769082d04bd13983720656975d98.png)'
-      }),
+      backgroundImage: {
+        'mobile-app': "url('https://landing-strapi-images-development.s3.eu-west-1.amazonaws.com/0.1.0/_next/static/media/header-background.9ad4769082d04bd13983720656975d98.png')",
+      },
+     
 
       colors: {
         'brand-color': '#4c3398',
