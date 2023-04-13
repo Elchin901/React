37272b46f72c3727e227c0adc870722f72072aa4
Elchin901 <73276152+Elchin901@users.noreply.github.com>
2023-04-13 07:34:54 +0200

    ok

--- project3/src/components/MobileApp.js
+++ project3/src/components/MobileApp.js
@@ -1,6 +1,25 @@
 
 export default function MobileApp() {
-    return (
-      <div>MobileApp</div>
-    )
-  }
+  return (
+    <div className="bg-mobile-app p-10">
+      <div>
+        <h3>Getir'i indirin</h3>
+        <p>isediginiz urunleri dakikalar icinde kapiniza getirelim.</p>
+        <nav>
+          <a href="#">
+            <img
+              src="https://getir.com/_next/image?url=https%3A%2F%2Flanding-strapi-images-development.s3.eu-west-1.amazonaws.com%2Fappstorees_3e5b4c3264.svg&w=256&q=75" />
+          </a>
+          <a href="#">
+            <img
+              src="https://getir.com/_next/image?url=https%3A%2F%2Flanding-strapi-images-development.s3.eu-west-1.amazonaws.com%2Fplaystorees_82f027bb11.svg&w=256&q=75" />
+          </a>
+          <a href="#">
+            <img
+              src="https://getir.com/_next/image?url=https%3A%2F%2Flanding-strapi-images-development.s3.eu-west-1.amazonaws.com%2Fplaystorees_82f027bb11.svg&w=256&q=75" />
+          </a>
+        </nav>
+      </div>
+    </div>
+  )
+}
--- project3/tailwind.config.js
+++ project3/tailwind.config.js
@@ -1,16 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode :'jit',
+  mode: 'jit',
   content: ["./src/**/*.{js,jsx,ts,tsx}", './public/index.html'],
   theme: {
     extend: {
-      spacing:{
-        '0.1':'0.063rem'},
+      spacing: {
+        '0.1': '0.063rem'
+      },
+
+      // backgroundImage: {
+      //   'mobile-app': "url('https://tailwindcss.com/_next/static/media/tailwindui-small@75.8bb955b2.jpg')",
+      // },
+      backgroundImage: theme => ({
+        'mobile-app': 'url(https://landing-strapi-images-development.s3.eu-west-1.amazonaws.com/0.1.0/_next/static/media/header-background.9ad4769082d04bd13983720656975d98.png)'
+      }),
+
       colors: {
-        'brand-color':'#4c3398',
-        'primary-brand-color':'#5d3ebc',
-        'second-brand-color':'#7849f7',
-        'brand-yellow':'#ffd300'
+        'brand-color': '#4c3398',
+        'primary-brand-color': '#5d3ebc',
+        'second-brand-color': '#7849f7',
+        'brand-yellow': '#ffd300'
       },
     },
   },
