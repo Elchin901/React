28971c8a13c2b74512f44e61aa0407fa3306deeb
Elchin901 <73276152+Elchin901@users.noreply.github.com>
2023-07-19 12:19:35 +0200

    ok

--- portfolio3.1/src/components/Main.js
+++ portfolio3.1/src/components/Main.js
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <div className='bg-primary'>
+      <div className='container'>
+        <div className='text-white font-kanit mx-auto'>
+          <p className='text-9xl'>Hello! I’m Gonagov Elchin — curious Front End Web Developer based in Baku,Azerbaijan 🌆. Interested in technology ,
+            hardware user interfaces, science & developer tools,
+            human interactions, productivity, ethics.
+          </p>
+        </div>
+        <div>
+
+        </div>
+
+      </div>
+    </div>
   )
 }
