077dc02e42d4495755a1bad23d1aaf8b2eb15686
Elchin901 <73276152+Elchin901@users.noreply.github.com>
2023-10-01 14:32:13 +0200

    ok

--- twitter-clone/src/layout/main/sidebar/menu/index.jsx
+++ twitter-clone/src/layout/main/sidebar/menu/index.jsx
@@ -1,6 +1,8 @@
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import classNames from "classnames";
 import { mainMenu } from "../../../../utils/consts";
+import More from "./more";
+import New from "./new";
 
 export default function Menu() {
 
@@ -10,16 +12,16 @@ export default function Menu() {
 
 			{mainMenu.map((menu, index) => (
 				<NavLink to={menu.path} className="py-[3px] block group">
-					{({isActive}) => (
+					{({ isActive }) => (
 						<div
 							className={classNames("p-3 rounded-full transition-colors inline-flex items-center gap-5 group-hover:bg-[color:var(--background-third)]", {
 								"font-bold": isActive
 							})}>
 							<div className="w-[26.25px] h-[26.25px] relative">
-									<span
-										className="w-[18px] h-[18px] rounded-full bg-[color:var(--color-primary)] text-[color:var(--background-primary)] border border-[color:var(--background-primary)] absolute -top-1.5 -right-1 flex items-center justify-center text-[11px]">
-										{menu?.notification}
-									</span>
+								<span
+									className="w-[18px] h-[18px] rounded-full bg-[color:var(--color-primary)] text-[color:var(--background-primary)] border border-[color:var(--background-primary)] absolute -top-1.5 -right-1 flex items-center justify-center text-[11px]">
+									{menu?.notification}
+								</span>
 								{!isActive && menu.icon.passive}
 								{isActive && menu.icon.active}
 							</div>
@@ -31,6 +33,8 @@ export default function Menu() {
 				</NavLink>
 			))}
 
+			<More />
+			<New />
 		</nav>
 	)
 }
