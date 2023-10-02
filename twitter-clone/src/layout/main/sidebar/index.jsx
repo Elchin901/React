96920e03399a59fc85292bb6769be6129cd3fcf3
Elchin901 <73276152+Elchin901@users.noreply.github.com>
2023-10-02 09:28:01 +0200

    ok

--- twitter-clone/src/layout/main/sidebar/index.jsx
+++ twitter-clone/src/layout/main/sidebar/index.jsx
@@ -3,9 +3,15 @@ import Menu from "./menu";
 
 export default function Sidebar() {
     return (
-        <aside className="w-[275px] min-h-screen ">
+        <aside className="w-[275px] min-h-screen max-h-screen px-2 flex flex-col overflow-auto ">
             <Logo/>
             <Menu/>
+
+            <div className="mt-auto">
+                <div className="my-3">
+                    test
+                </div>
+            </div>
         </aside>)
 
 }
