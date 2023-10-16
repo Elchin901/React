9c53dcb4349e02629b5597ca46d896f4e86b34df
Elchin901 <73276152+Elchin901@users.noreply.github.com>
2023-10-16 11:41:26 +0200

    ok

--- twitter-clone/src/layout/main/sidebar/index.jsx
+++ twitter-clone/src/layout/main/sidebar/index.jsx
@@ -1,15 +1,16 @@
 import Logo from "./logo";
 import Menu from "./menu";
+import Account from "./account";
 
 export default function Sidebar() {
     return (
         <aside className="w-[275px] min-h-screen max-h-screen px-2 flex flex-col overflow-auto ">
             <Logo/>
             <Menu/>
+            <Account/>
 
             <div className="mt-auto">
                 <div className="my-3">
-                    test
                 </div>
             </div>
         </aside>)
--- twitter-clone/src/store/auth/index.js
+++ twitter-clone/src/store/auth/index.js
@@ -4,9 +4,24 @@ const initialState ={
     currentAccount:{
         id:1,
         username:'elchin',
+        fullName:'#gonagov_l',
         avatar:'https://preview.redd.it/hot-take-g5-as-a-transformation-is-amazing-and-luffy-being-v0-gksntq6pnchb1.jpg?width=640&crop=smart&auto=webp&s=0ae16feb011b924ee9e78fa260de650e69a17329'
     },
-    accounts : []
+    accounts : [
+        {
+            id:1,
+            username:'elchin',
+            fullName:'#gonagov_l',
+            avatar:'https://preview.redd.it/hot-take-g5-as-a-transformation-is-amazing-and-luffy-being-v0-gksntq6pnchb1.jpg?width=640&crop=smart&auto=webp&s=0ae16feb011b924ee9e78fa260de650e69a17329'
+        },
+        {
+            id:1,
+            username:'luffy',
+            fullName:'#monkey.d.luffy',
+            avatar:'https://lh3.googleusercontent.com/ogw/AKPQZvwakGwK_hL40s25N--pBPdu5yYRNedI4Z5nLPPBbw=s32-c-mo'
+        },
+    ]
+
 }
 
 const auth = createSlice({
