c0a76d5f9b5fc2459df86b92059dcf88351ec616
Elchin901 <73276152+Elchin901@users.noreply.github.com>
2023-10-23 12:42:21 +0200

    ok

--- portfolio5/src/components/About/About.jsx
+++ portfolio5/src/components/About/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const About = () => {
   return (
-    <div>
+    <div className='flex flex-wrap'>
       <h2>About</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ducimus placeat cupiditate temporibus. Totam exercitationem odio ratione minus. Molestiae asperiores nostrum cum ut accusamus. Deserunt inventore voluptas debitis laboriosam quidem?</p>
     </div>
