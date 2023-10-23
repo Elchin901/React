c4edec628cfc8d87d8dd48bede65f44354c9270b
Elchin901 <73276152+Elchin901@users.noreply.github.com>
2023-10-23 13:04:35 +0200

    ok

--- portfolio5/src/components/Navbar/Navbar.jsx
+++ portfolio5/src/components/Navbar/Navbar.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <div className='text-rose-600 flex justify-center '>
-      <div className='flex pt-20'>
+    <div className='text-rose-600 pb-10 '>
+      <div className='flex justify-start pt-20'>
         <div>
           <img className='w-[100px] rounded-full' src='https://preview.redd.it/hot-take-g5-as-a-transformation-is-amazing-and-luffy-being-v0-gksntq6pnchb1.jpg?width=640&crop=smart&auto=webp&s=0ae16feb011b924ee9e78fa260de650e69a17329'></img>
         </div>
-        <div className='mx-auto pt-5'>
+        <div className='pt-6 pl-3'>
           <h1>Elchin Gonagov</h1>
           <p>Front-End Developer</p>
         </div>
