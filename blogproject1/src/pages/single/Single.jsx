f9fe9f7453d3fc40e030123dab12def5169649db
Elchin901 <73276152+Elchin901@users.noreply.github.com>
2024-02-13 09:01:21 +0100

    ok

--- blogproject1/src/pages/single/Single.jsx
+++ blogproject1/src/pages/single/Single.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar'
+import SinglePots from '../../components/singlePost/SinglePots'
+import "./single.css"
 
 export default function Single() {
   return (
-    <div></div>
+    <div className='single'>
+      <SinglePots/>
+      <Sidebar/>
+    </div>
   )
 }
