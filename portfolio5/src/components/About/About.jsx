97cb48d2c3db54e34f76d85bb2d9bf30d3a6b2e5
Elchin901 <73276152+Elchin901@users.noreply.github.com>
2024-03-21 11:04:01 +0100

    ok

--- portfolio5/src/components/About/About.jsx
+++ portfolio5/src/components/About/About.jsx
@@ -4,7 +4,7 @@ export const About = () => {
   return (
     <div className="flex flex-col">
       <h2 className='py-2 color2 poppins-medium '>About</h2>
-      <p className='color3 w-fit poppins-regular'>Enthusiastic and dedicated junior web developer with a passion for crafting clean, efficient, and user-friendly web experiences. Proficient in HTML, CSS, and JavaScript, with a solid understanding of modern web development practices and technologies. Eager to contribute my skills and creativity to dynamic projects while continuously learning and growing in a collaborative team environment.</p>
+      <p className='color3 w-fit poppins-regular'>Enthusiastic and dedicated junior front-end developer with a passion for crafting clean, efficient, and user-friendly web experiences. Proficient in HTML, CSS, and JavaScript, with a solid understanding of modern web development practices and technologies. Eager to contribute my skills and creativity to dynamic projects while continuously learning and growing in a collaborative team environment.</p>
     </div>
   )
 }
--- portfolio5/src/components/Work/Work.jsx
+++ portfolio5/src/components/Work/Work.jsx
@@ -9,28 +9,28 @@ export const Work = () => {
       <div className='flex py-10'>
         <img src={getImageUrl('img/web1.png')} className='h-32'></img>
         <div className='px-3'>
-          <h4 className='color2 poppins-regular'>Photo.html</h4>
+          <h4 className='color2 poppins-regular'><a href='https://elchin901.github.io/web1/Photo.html'>Photo.html</a></h4>
           <p className='color3 text-[12px] poppins-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt corporis ipsa, repellat odio,res exercitationem dolore praesentium deserunt?</p>
         </div>
       </div>
       <div className='flex py-10'>
         <img src={getImageUrl('img/web2.png')} className='h-32'></img>
         <div className='px-3'>
-          <h4 className='color2 poppins-regular'>Photo.html</h4>
+          <h4 className='color2 poppins-regular'><a href='https://elchin901.github.io/web2/Flowie.html'>Flowers</a></h4>
           <p className='color3 text-[12px] poppins-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt corporis ipsa, repellat odio,res exercitationem dolore praesentium deserunt?</p>
         </div>
       </div>
       <div className='flex py-10'>
         <img src={getImageUrl('img/web3.png')} className='h-32'></img>
         <div className='px-3'>
-          <h4 className='color2 poppins-regular'>Photo.html</h4>
+          <h4 className='color2 poppins-regular'><a href='https://elchin901.github.io/web3/Portland.html'>Phones</a></h4>
           <p className='color3 text-[12px] poppins-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt corporis ipsa, repellat odio,res exercitationem dolore praesentium deserunt?</p>
         </div>
       </div>
       <div className='flex py-10'>
         <img src={getImageUrl('img/web4.png')} className='h-32'></img>
         <div className='px-3'>
-          <h4 className='color2 poppins-regular'>Photo.html</h4>
+          <h4 className='color2 poppins-regular'><a href='https://elchin901.github.io/web4/Spotify.html'>Spotify Clone</a></h4>
           <p className='color3 text-[12px] poppins-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt corporis ipsa, repellat odio,res exercitationem dolore praesentium deserunt?</p>
         </div>
       </div>
