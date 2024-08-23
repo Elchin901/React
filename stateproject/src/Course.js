7dff008e931871c09107f992b7fa333b01767fcf
Elchin901 <73276152+Elchin901@users.noreply.github.com>
2024-08-23 23:10:22 +0200

    ok

---
+++ stateproject/src/Course.js
@@ -0,0 +1,17 @@
+import Javascript from "./images/javascript.png"
+import Nodejs from "./images/nodejs.png"
+import Typescript from "./images/typescript.png"
+import Vite from "./images/vite.png"
+
+
+function Course({courseName}) {
+  console.log(Javascript);
+  console.log(courseName);
+  return (
+    <div>
+      <img src="Javascript" alt="" />
+    </div>
+  )
+}
+
+export default Course;
